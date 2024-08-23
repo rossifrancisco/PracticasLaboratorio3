c1a4211ea2d4a5b06d1db9a484bc872480a991df
francisco <rossi.francisco.64@gmail.com>
2024-08-23 16:44:33 -0300

    Practica 1 Terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
+// Dada la siguiente watchlist, crear una función que tome el array original
+// y devuelva un arreglo de objetos que solo contenga las películas:
+// 1. Dirigidas por Cristopher Nolan.
+// 2. Con un imdbRating > 8.0
+// El arreglo que se devuelve SOLO DEBE CONTENER el nombre de la película, ningún otra información
+// Es obligatorio para este CC resolverlo con la función map()
+
 import './App.css'
+import WatchList from './components/watchList/WatchList';
 
 function App() {
+  const returnMovieTitle = (watchlist) => {
+    return watchlist.map((movie) => {
+      if (movie.Director === 'Christopher Nolan' && movie.imdbRating > 8.0) {
+        return movie.Title;
+      } else {
+        return null;
+      }
+    }).filter((title) => title !== null);
+  };
+
+  const titlesArray = returnMovieTitle(WatchList());
+  console.log(titlesArray);
+
   return (
     <div>
-      
+      <h1>Películas Filtradas</h1>
+      <ul>
+        {titlesArray.map((title, index) => (
+          <li key={index}>{title}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
